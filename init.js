d2ae7c3b41333b133821fe92da7a13a9433a7a32
Matias <carreamatias@gmail.com>
2020-10-02 17:13:56 -0300

    v0

---
+++ init.js
@@ -0,0 +1,8 @@
+
+// Nuevo contador desde 58 dias 16 horas
+var flipdown = new FlipDown( 1605884400, {
+    theme: 'light' // or dark
+} );
+// Cuenta para dentro de 2 dias (new Date().getTime() / 1000) + (86400 * 2) + 1
+
+flipdown.start();
